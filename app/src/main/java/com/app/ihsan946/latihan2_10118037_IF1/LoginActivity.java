842075e20184fb91a0e95f38a06a8431c948ab3a
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-04-24 20:43:08 +0700

    Tampilan Login

---
+++ app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/LoginActivity.java
@@ -0,0 +1,21 @@
+//NIM : 10118037
+//Nama : Muhammad Ihsan
+//Kelas : IF-1/S1/VI
+//copyright 2021
+
+package com.app.ihsan946.latihan2_10118037_IF1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
