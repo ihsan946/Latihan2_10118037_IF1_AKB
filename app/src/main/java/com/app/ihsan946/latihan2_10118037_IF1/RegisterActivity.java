b1ec28d4121a21fe345ab055f292e78eae7e7202
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-04-24 23:17:59 +0700

    Tampilan Register dan transisi dari Login Ke Register

--- app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/RegisterActivity.java
+++ app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/RegisterActivity.java
@@ -1,8 +1,3 @@
-//NIM : 10118037
-//Nama : Muhammad Ihsan
-//Kelas : IF-1/S1/VI
-//copyright 2021
-
 package com.app.ihsan946.latihan2_10118037_IF1;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 
-    public void pindahRegister(View view){
-        Intent intent = new Intent(this,RegisterActivity.class);
+    public void pindahLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
 
     }
-
 
 }
