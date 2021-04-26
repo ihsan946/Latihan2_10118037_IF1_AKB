c79bbf47194b55762b199ef4131e41a6d5299601
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-04-26 11:09:47 +0700

    Tampilan Main Home, transisi dari Almost there ke verify, transisi dari Verify ke Main

--- app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/AlmostActivity.java
+++ app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/AlmostActivity.java
@@ -2,7 +2,9 @@ package com.app.ihsan946.latihan2_10118037_IF1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AlmostActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AlmostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_almost);
+    }
+
+    public void pindahVerify(View view){
+        Intent intent = new Intent(this, VerifyActivity.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/VerifyActivity.java
+++ app/src/main/java/com/app/ihsan946/latihan2_10118037_IF1/VerifyActivity.java
@@ -2,7 +2,9 @@ package com.app.ihsan946.latihan2_10118037_IF1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class VerifyActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class VerifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+    }
+
+    public void pindahMain(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
